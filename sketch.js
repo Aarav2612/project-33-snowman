689993fcbbf5cfa9ebb77602478a30ad586e3c6e
Aarav2612 <89640281+Aarav2612@users.noreply.github.com>
2022-01-03 22:01:03 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+  image = loadImage ("snow2.png")
+}
+
+function draw() {
+  background("white");  
+  background ("snow2.png")
+  
+  drawSprites();
+}
+
+
+
+
+
+
+
+
+
+
+``
